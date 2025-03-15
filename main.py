fa0cae43844a9f3fbf860dc9d60de26f00d3f5ee
= <=>
2025-03-15 23:10:51 +0100

    some fixes some updates

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route('/events')
 def events():
     df = fastf1.get_event_schedule(2024)
@@ -15,64 +16,38 @@ def events():
 
     return jsonify(df.to_dict(orient="records"))
 
+
 @app.route('/events/<year>/<name>')
 def event(year, name):
     session = fastf1.get_session(int(year), name, 'R')
-    session.load()
-    return jsonify({"year": year, "name": name})
+    session.load(telemetry=True)
 
+    drivers = [session.get_driver(driver).Abbreviation for driver in session.drivers]
+
+    res = []
+
+    for driver in drivers:
+        driver_laps = session.laps.pick_drivers([driver])
+        telemetry = driver_laps.get_telemetry()
+
+        dict_arr = telemetry.to_dict(orient="records")
+
+        for i in range(len(dict_arr)):
+            formatted_step = format_telemetry_step(dict_arr[i], driver)
+
+            res.append([formatted_step]) if i >= len(res) else res[i].append(formatted_step)
+
+    return jsonify(res)
+
+
+def format_telemetry_step(telemetry_step: dict, driver: str) -> dict:
+    return {"Brake": telemetry_step["Brake"],
+            "DRS": telemetry_step["DRS"],
+            "RPM": telemetry_step["RPM"],
+            "X": telemetry_step["X"],
+            "Y": telemetry_step["Y"],
+            "Driver": driver}
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    # x = fastf1.get_event_schedule(2024)
-
-
-    # def init():
-    #     ax.set_xlim(-10_000, 10_000)
-    #     ax.set_ylim(-10_000, 10_000)
-    #     return ln,
-    #
-    #
-    # def step(i: int):
-    #     ln.set_data([x[i + 2]], [y[i + 2]])
-    #     return ln,
-    #
-    # session = fastf1.get_session(2024, 'Austrian Grand Prix', 'R')
-    # session.load()
-    #
-    # laps = session.laps.pick_drivers(["VER"])
-    # x, y = laps.telemetry["X"], laps.telemetry["Y"]
-    #
-    # fig, ax = plt.subplots()
-    # xdata, ydata = [], []
-    # ln, = plt.plot([], [], 'ro')
-    #
-    # ani = FuncAnimation(fig, step, frames=2000, init_func=init)
-    # plt.show()
-
-
-
-
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from matplotlib.animation import FuncAnimation
-#
-# fig, ax = plt.subplots()
-# xdata, ydata = [], []
-# ln, = plt.plot([], [], 'ro')
-#
-# def init():
-#     ax.set_xlim(-10, 10)
-#     ax.set_ylim(-10, 10)
-#     return ln,
-#
-# def update(frame: float):
-#     ln.set_data([1 * frame], [2*frame])
-#     return ln,
-#
-# ani = FuncAnimation(fig, update, frames=200, init_func=init)
-# plt.show()
-#
-#
