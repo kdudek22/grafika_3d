3bad843bc5cf2dbf0f6bcfd8dcd4ed1d66b692e9
kdudek22 <appleofeden8686@gmail.com>
2025-03-29 20:30:11 +0100

    nice changes before web socket integration

--- Backend/main.py
+++ Backend/main.py
@@ -1,26 +1,37 @@
 import time
 import json
 from utils import get_physics
+from flask import Flask
 
 
 if __name__ == "__main__":
-    pos_x, pos_y, pos_z = [], [], []
+    data = []
 
     last = time.time()
     first = time.time()
 
     while True:
-        if time.time() - last > 0.3:
+        if time.time() - last > 0.05:
             physics_reading = get_physics()
-            print(f"\r{physics_reading.SpeedKmh}", end='', flush=True)
+            print(f"\r{physics_reading.Heading}", end='', flush=True)
+
+            res = {
+                   "x": physics_reading.TyreContactPoint.FL.x,
+                   "y": physics_reading.TyreContactPoint.FL.y,
+                   "z": physics_reading.TyreContactPoint.FL.z,
+                   "heading": physics_reading.Heading
+                }
+
+            data.append(res)
 
             last = time.time()
 
-        if time.time() - first > 120:
-            res = {"x": pos_x, "y": pos_y, "z": pos_z}
+        if time.time() - first > 300:
 
-            with open("data.json", "w") as f:
-                json.dump(res, f)
+            with open("data_bmw_20fps.json", "w") as f:
+                json.dump(data, f)
 
             print("Saved File")
             exit()
+
+
