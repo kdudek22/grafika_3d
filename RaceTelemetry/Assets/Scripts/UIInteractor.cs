7d4662302bec8d9cce5c1b7370577ed888b1a6ef
kdudek22 <appleofeden8686@gmail.com>
2025-05-16 19:16:06 +0200

    added missing files

---
+++ RaceTelemetry/Assets/Scripts/UIInteractor.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIInteractor : MonoBehaviour
+{
+    public static UIInteractor instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void UpdateUI(Reading reading)
+    {
+        UIDocument doc = GetComponent<UIDocument>();
+        if (doc == null)
+        {
+            Debug.LogError("UIDocument not found on GameObject.");
+            return;
+        }
+
+        VisualElement root = doc.rootVisualElement;
+        if (root == null)
+        {
+            Debug.LogError("Root VisualElement is null.");
+            return;
+        }
+
+        Label speed = root.Q<Label>("Speed");
+        Label gear = root.Q<Label>("Gear");
+        Label rpm = root.Q<Label>("RPM");
+
+        speed.text = reading.speed.ToString();
+        gear.text = reading.gear.ToString();
+        rpm.text = reading.rmp.ToString();
+    }
+}
