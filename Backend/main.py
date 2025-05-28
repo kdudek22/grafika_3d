cb2737fdfa040385649948ca52a64f65f41bfa4c
kdudek22 <appleofeden8686@gmail.com>
2025-05-28 14:13:28 +0200

    more cleanup

--- Backend/main.py
+++ Backend/main.py
@@ -12,43 +12,6 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*', async_mode="threading")
 
 
-def save_data(seconds=300, timestep=0.05):
-    res = []
-
-    start, last = time.time(), time.time()
-
-    while time.time() - start < seconds:
-        if time.time() - last >= timestep:
-
-            graphics_reading = get_graphics()
-            physics_reading = get_physics()
-
-            res.append({
-                "x": (physics_reading.TyreContactPoint.FL.x + physics_reading.TyreContactPoint.RR.x) / 2,
-                "y": (physics_reading.TyreContactPoint.FL.y + physics_reading.TyreContactPoint.RR.y) / 2,
-                "z": (physics_reading.TyreContactPoint.FL.z + physics_reading.TyreContactPoint.RR.z) / 2,
-                "heading": physics_reading.Heading,
-                "pitch": physics_reading.Pitch,
-                "roll": physics_reading.Roll,
-                "gear": physics_reading.Gear,
-                "rpm": physics_reading.Rpm,
-                "steer_angle": physics_reading.SteerAngle,
-                "speed": physics_reading.SpeedKmh,
-                "gas": physics_reading.Gas,
-                "break": physics_reading.Brake,
-                "velocity_vector": [physics_reading.Velocity.x, physics_reading.Velocity.y, physics_reading.Velocity.z],
-                "current_time": graphics_reading.currentTime,
-            })
-
-            print(res[-1])
-
-            last = time.time()
-
-    with open("bmw_data.json", "w") as f:
-        json.dump(res, f, indent=4)
-        print("Saved file")
-
-
 def get_telemetry_data():
     graphics_reading = get_graphics()
     physics_reading = get_physics()
@@ -71,6 +34,23 @@ def get_telemetry_data():
     }
 
 
+def save_data(seconds=300, timestep=0.05):
+    res = []
+
+    start, last = time.time(), time.time()
+
+    while time.time() - start < seconds:
+        if time.time() - last >= timestep:
+
+            res.append(get_telemetry_data())
+
+            last = time.time()
+
+    with open("bmw_data.json", "w") as f:
+        json.dump(res, f, indent=4)
+        print("Saved file")
+
+
 @app.route("/telemetry")
 def live_data():
     return jsonify(get_telemetry_data())
@@ -87,6 +67,7 @@ def handle_connect():
             socketio.sleep(0.05)
 
     threading.Thread(target=send_telemetry, daemon=True).start()
+
 
 if __name__ == '__main__':
     socketio.run(app, host='127.0.0.1', port=5000)
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # Unity Assetto Corsa data visualization
 
 ![Funny cat](assets/2025-05-2714-50-38_Clip_smallest-ezgif.com-video-to-gif-converter.gif)
+
+This project enables you to visualize live data from Assetto Corsa using shared memory.
+Data from the shared memory is exposed from a socket-io thats running with flask.
+
+
+
+
+Map models are from game files, and the E30 model is a free model from sketchfab.
